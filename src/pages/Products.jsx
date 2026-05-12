4f295724c88ec30a3c68051858851e0a2a74bc6e
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2026-05-12 14:38:12 +0200

    changed name in store folder

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,20 @@
-
-
+import useProducts from '../hooks/useProducts'
 
 function Products() {
-  return <div> Products </div>
+  const { products, loading } = useProducts()
+
+  if (loading) return <p>Laddar produkter...</p>
+
+  return (
+    <div>
+      {products.map(product => (
+        <div key={product.id}>
+          <p>{product.name}</p>
+          <p>{product.price} kr</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Products
