3efb2c25f523d01f12a64e7723c339f4d409d0b9
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2026-05-12 15:07:14 +0200

    added more files and removed one

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { HashRouter, Routes, Route } from 'react-router'
-import Navbar from './components/NavBar.jsx'
-import Home from './pages/Home.jsx'
-import Products from './pages/Products.jsx'
-import Cart from './pages/Cart.jsx'
-import Login from './pages/Login.jsx'
-import Admin from './pages/Admin.jsx'
+import { HashRouter, Routes, Route } from "react-router";
+
+import MainLayout from "./layout/MainLayout";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
+
+        <Route element={<MainLayout />}>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+
+        </Route>
+
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
+
       </Routes>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
