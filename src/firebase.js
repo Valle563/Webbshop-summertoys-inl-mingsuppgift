cb48791dcc930ba7cebc65159a71062acf9fab9d
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2026-05-17 09:40:42 +0200

    flytta firebase-nycklar till env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,31 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+// import { initializeApp, getApps, getApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCZ0E-SpZJ2vMgwZXtrMMIUJUHBL9fM1sk",
+//   authDomain: "valentinos-projects.firebaseapp.com",
+//   projectId: "valentinos-projects",
+//   storageBucket: "valentinos-projects.firebasestorage.app",
+//   messagingSenderId: "277812146896",
+//   appId: "1:277812146896:web:50b90f110d8387176ea4d5",
+// };
+
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
+
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ0E-SpZJ2vMgwZXtrMMIUJUHBL9fM1sk",
-  authDomain: "valentinos-projects.firebaseapp.com",
-  projectId: "valentinos-projects",
-  storageBucket: "valentinos-projects.firebasestorage.app",
-  messagingSenderId: "277812146896",
-  appId: "1:277812146896:web:50b90f110d8387176ea4d5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
